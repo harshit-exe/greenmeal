b54fc4ce405d51d14dbaeb2f9dc21e6cd7f8232d
harshit-exe <nikamharshit55@gmail.com>
2024-11-09 16:09:24 +0530

    fix bug

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,10 @@
+import Hero from "@/components/Home/Hero";
 import Image from "next/image";
 
-import Hero from "@/components/Home/Hero";
 export default function Home() {
   return (
     <>
-      <Hero />
+      <Hero/>
     </>
   );
 }
